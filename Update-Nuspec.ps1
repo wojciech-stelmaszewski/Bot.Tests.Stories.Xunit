df4a6d3886c1366dfb1c24c7dd5f2880f612d6f0
Jakub Kaczmarek <jkaczmarek2@objectivity.co.uk>
2017-10-19 09:42:20 +0200

    Include nuspec update script, custom variables, update anayzers ruleset and appveyor config

---
+++ Update-Nuspec.ps1
@@ -0,0 +1,7 @@
+﻿param([string]$version=1.0.0)
+$sfbDir = (get-item $PSScriptRoot )
+$appConfigFile = [IO.Path]::Combine($sfbDir, 'Objectivity.Bot.Tests.Stories.Xunit\Objectivity.Bot.Tests.Stories.Xunit.nuspec')
+$appConfig = New-Object XML;
+$appConfig.Load($appConfigFile);
+$appConfig.package.metadata.version = "$version";
+$appConfig.Save($appConfigFile);
