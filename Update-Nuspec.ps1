40daa469a7570c8a69c69e0c8de0bf9155208e70
Jacek Kolonko <jkolonko@objectivity.co.uk>
2017-12-22 16:07:21 +0100

    Describe package in csproj, remove unused nuspec file

--- Update-Nuspec.ps1
+++
@@ -1,7 +0,0 @@
-﻿param([string]$version=1.0.0)
-$sfbDir = (get-item $PSScriptRoot )
-$appConfigFile = [IO.Path]::Combine($sfbDir, 'Objectivity.Bot.Tests.Stories.Xunit\Objectivity.Bot.Tests.Stories.Xunit.nuspec')
-$appConfig = New-Object XML;
-$appConfig.Load($appConfigFile);
-$appConfig.package.metadata.version = "$version";
-$appConfig.Save($appConfigFile);
